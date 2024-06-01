74af89f7f24f7c81222b33aaf04b0cd7c7b48e80
talash-naeem <naeemtalash153@gmail.com>
2024-06-01 17:50:11 +0500

    section 10 finished

---
+++ src/test/java/com/talashCode/ecommerce/EcommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talashCode.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
