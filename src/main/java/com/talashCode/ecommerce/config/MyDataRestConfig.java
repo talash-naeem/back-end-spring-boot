15710c4c3075f76462aac425c1046ab53de4db79
talash-naeem <naeemtalash153@gmail.com>
2024-06-10 11:17:06 +0500

    change something in application.properties

--- src/main/java/com/talashCode/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/talashCode/ecommerce/config/MyDataRestConfig.java
@@ -3,14 +3,25 @@ package com.talashCode.ecommerce.config;
 
 import com.talashCode.ecommerce.entity.Product;
 import com.talashCode.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,6 +39,36 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        exposedIds(config);
+    }
+    private void exposedIds(RepositoryRestConfiguration config){
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
+        List<Class> entitiesClasses = new ArrayList<>();
+
+        for (EntityType tempEntitytype : entities){
+            entitiesClasses.add(tempEntitytype.getJavaType());
+        }
+        Class[] domainTypes = entitiesClasses.toArray(entitiesClasses.toArray(new Class[0]));
+        config.exposeIdsFor(domainTypes);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
